7a3ed6f0613c1c7b4713c8a5ecf3f5ba193f837a
Tim Jacomb <tim.jacomb@hmcts.net>
2020-07-28 08:02:25 +0100

    Automate updating Jenkins plugins (#59)

---
+++ bin/update-plugins.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "Updating plugins"
+
+# stty command is needed due to https://github.com/moby/moby/issues/37366#issuecomment-401157643
+docker-compose run jenkins bash -c "stty -onlcr && java -jar /usr/local/lib/jenkins-plugin-manager.jar -f /usr/share/jenkins/ref/plugins.txt --available-updates --output txt" > jenkins/plugins2.txt
+
+mv jenkins/plugins2.txt jenkins/plugins.txt
+
+git diff jenkins/plugins.txt
+
+echo "Updating plugins complete"
