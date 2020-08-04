7c970298594e1bcdff85a9709ecb4396d66c6b51
Luigi Bitonti <luigi.bitonti@hmcts.net>
2020-08-04 15:37:40 +0100

    Add acr task file. Base build agent on minimal one

--- bin/acr-task-creation.sh
+++ bin/acr-task-creation.sh
@@ -5,19 +5,9 @@ export GIT_PAT=$(az keyvault secret show --vault-name infra-vault-prod --name hm
 
 az acr task create \
   --registry $ACR_NAME \
-  --name jenkins-jnlp-agent \
-  --image jenkins/jnlp-agent:{{.Run.ID}} \
-  --context https://github.com/hmcts/cnp-jenkins-docker.git#master:jenkins-jnlp-agent \
-  --file Dockerfile \
-  --git-access-token $GIT_PAT \
-  --subscription DCD-CNP-PROD
-
-az acr task create \
-  --registry $ACR_NAME \
-  --name jenkins-inbound-agent \
-  --image jenkins/inbound-agent:{{.Run.ID}} \
-  --context https://github.com/hmcts/cnp-jenkins-docker.git#master:jenkins-inbound-agent \
-  --file Dockerfile \
+  --name jenkins-agent \
+  --context https://github.com/hmcts/cnp-jenkins-docker.git#master:jenkins-agent \
+  --file acr-agents-build-task.yaml \
   --git-access-token $GIT_PAT \
   --subscription DCD-CNP-PROD
 
