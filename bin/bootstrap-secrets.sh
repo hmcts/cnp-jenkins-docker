f6f4ebea0dfaea57e4c38d56c3b6c3ca4e9815e7
Tim Jacomb <tim.jacomb@hmcts.net>
2022-04-20 16:21:49 +0100

    DTSPO-7559 Update secrets for GitHub apps

--- bin/bootstrap-secrets.sh
+++ bin/bootstrap-secrets.sh
@@ -23,30 +23,6 @@ function readSecret () {
 
 readSecret slack-token
 
-readSecret hmcts-github-apikey-cmc
-readSecret hmcts-github-apikey-div
-readSecret hmcts-github-apikey-finrem
-readSecret hmcts-github-apikey-cdm
-readSecret hmcts-github-apikey-iac
-readSecret hmcts-github-apikey-platform
-readSecret hmcts-github-apikey-rpa
-readSecret hmcts-github-apikey-sscs
-readSecret hmcts-github-apikey-probate
-readSecret hmcts-github-apikey-feepay
-readSecret hmcts-github-apikey-sl
-readSecret hmcts-github-apikey-cnp
-readSecret hmcts-github-apikey-idam
-readSecret hmcts-github-apikey-cet
-readSecret hmcts-github-apikey-fpl
-readSecret hmcts-github-apikey-am
-readSecret hmcts-github-apikey-ethos
-readSecret hmcts-github-apikey-ctsc
-readSecret hmcts-github-apikey-bsp
-readSecret hmcts-github-apikey-rd
-readSecret hmcts-github-apikey-mi
-
-readSecret hmcts-github-apikey
-
 readSecret mgmt-bastion-creds-password
 
 readSecret sonarcloud-api-token
@@ -56,11 +32,10 @@ readSecret jenkins-build-logs-key
 
 readSecret sauce-access-key
 
-readSecret bfa-user-password
-
 readSecret jenkins-sp-subscription-id
 readSecret jenkins-sp-client-id
 readSecret jenkins-sp-client-secret
 readSecret jenkins-sp-tenant-id
-readSecret hmcts-jenkins-rpe-ghapp
+
+readSecret hmcts-jenkins-cft-ghapp
 readSecret hmcts-jenkins-cnp-ghapp
